793388894caab2d61846f02b8309e822ad7d073f
Marcel Benders <marcel.benders@cleverbridge.com>
2020-01-16 18:01:40 +0100

    (dev) Add React

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +27,12 @@ namespace TestEndpoint
             services.AddScoped<IDockerImageRepository, DockerImageRepository>();
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
             services.AddControllers();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
         public void Configure(
@@ -48,12 +55,22 @@ namespace TestEndpoint
                         LabelNames = new[] { "code", "method" }
                     });
             });
+            app.UseStaticFiles();
             UpdateDatabase(app);
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
 
